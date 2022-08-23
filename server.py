7dbddcbe6f0e305e4febeb3afefae22bfef67fc4
mmd <mmd.mostafaee@gmail.com>
2022-08-24 00:05:33 +0430

    Update data file Add cors to flask

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 import binascii
 from flask import Flask
-
+from flask_cors import CORS
 from generate_tree import generate_tree
 
 tree, data = generate_tree()
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/root/")
@@ -20,8 +21,7 @@ def get_root():
 @app.route("/proof/<tokenId>/")
 def get_proof(tokenId):
     amount = int(data[tokenId])
-    proof =  ["0x" + binascii.hexlify(p).decode() for p in tree.get_proof({'tokenId': tokenId, 'amount': amount})]
-    # print(tree.verify({'tokenId': tokenId, 'amount': amount}, proof))
+    proof = ["0x" + binascii.hexlify(p).decode() for p in tree.get_proof({'tokenId': tokenId, 'amount': amount})]
     return {
         'proof': proof,
         'amount': amount,
