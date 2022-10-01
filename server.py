5774f0760a167ee9e030ee7509d4e86ce6bbfef7
mmd <mmd.mostafaee@gmail.com>
2022-10-01 15:25:29 +0330

    Merge servers

--- server.py
+++ server.py
@@ -1,14 +1,13 @@
 import binascii
-from heapq import merge
 from flask import Flask
 from flask_cors import CORS
 from generate_nft_tree import generate_nft_tree
-
-# dei_tree, dei_data = generate_dei_tree()
-nft_tree, nft_data = generate_nft_tree()
+from generate_max_active_liquidity_tree import generate_tree
 
 app = Flask(__name__)
 CORS(app)
+
+nft_tree, nft_data = generate_nft_tree()
 
 
 @app.route("/nft/root/")
@@ -33,8 +32,40 @@ def get_nft_proof(token_id):
 
     proof = nft_tree.get_proof(el)
     proof = ["0x" + binascii.hexlify(p).decode() for p in proof]
+
     result = {
         "proof": proof,
-        "value": token_data,
+        "value": {
+            "tokenId": token_id,
+            "amount": str(amount),
+            "maturity_time": maturity_date,
+        },
+    }
+    return result
+
+
+tree, data = generate_tree()
+
+
+@app.route("/liquidity/root/")
+def get_root():
+    for _, node in tree.tree.items():
+        if node["parent"] is None:
+            return {"root": binascii.hexlify(node["hash"]).decode()}
+
+
+@app.route("/liquidity/proof/<address>/")
+def get_proof(address):
+    value = data[address]
+
+    el = {
+        "address": address,
+        "value": value,
+    }
+    proof = tree.get_proof(el)
+    proof = ["0x" + binascii.hexlify(p).decode() for p in proof]
+    result = {
+        "proof": proof,
+        "value": str(value),
     }
     return result
